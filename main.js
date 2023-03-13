c439df96dc13f526eb65cf854f4e095b6a74389e
Dj-Rom <118632628+Dj-Rom@users.noreply.github.com>
2023-03-13 22:02:25 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,82 +1,47 @@
 let userFirstName,
-        userFirst,
-        userLastName,
-        userLast,
-        userSecondName,
-        userSecond,
-        userAge,
-        userAg,
-        userGat,
-        userP,
-        userResult;
+    userFirst,
+    userLastName,
+    userLast,
+    userSecondName,
+    userSecond,
+    userAge,
+    userAg,
+    userGat,
+    userP,
+    userResult;
     
-    function UserFirstName() {
-        userFirstName = prompt('Введите Ваше имя : ');
-        
-        while (!userFirstName || !isNaN(userFirstName)
-        ) {
-            UserFirstName();
-        }
-    }
-    function UserLastName() {
-        userLastName = prompt('Введите Вашу фамилию:');
-        
-        while (!userLastName || !isNaN(userLastName)
-        ) {
-            UserLastName();
-        }
-    }
-    function UserSecondName() {
-        userSecondName = prompt('Введите Ваше отчество: ');
-        
-        while (!userSecondName || !isNaN(userSecondName)) {
-            UserSecondName();
-        }
-    }
+do { userFirstName = prompt('Введите Ваше имя : ');
+   } while (!userFirstName || !isNaN(userFirstName));
+do {userLastName = prompt('Введите Вашу фамилию:');
+   } while (!userLastName || !isNaN(userLastName));
+do{ userSecondName = prompt('Введите Ваше отчество: ');
+   } while (!userSecondName || !isNaN(userSecondName));
+do {userAge = prompt('Введите ваш Возраст :');
+   userAg = parseInt(userAge);
+} while (isNaN(userAg));
     
+userGat = confirm(`Ваш пол мужской?
+    ОК = ДА ,a отмена = НЕТ`);
+    userP = (userGat)? 63 : 58;
+    userResult = (userAg >= userP) ? 'Да' : 'нет';
+
     
-    function UserAge() {
-        userAge = prompt('Введите ваш Возраст :');
-        userAg = parseInt(userAge);
-        
-        while (isNaN(userAg)) {
-            UserAge();
-        }
+function replaceEnterUser(){ 
+    userFirst = userFirstName.replace(/[^a-zа-яA-ZА-Я]+/g, ''),
+    userLast = userLastName.replace(/[^a-zа-яA-ZА-Я]+/g, ''),
+    userSecond = userSecondName.replace(/[^a-zа-яA-ZА-Я]+/g, ''),
+    userAge = userAge.replace(/[^0-9]/g, ''); 
     }
     
-    function UserGender() {
-        userGat = confirm(`Ваш пол мужской?
-         ОК = ДА ,a отмена = НЕТ`);
-        userP = (userGat)? 63 : 58;
-        userResult = (userAg >= userP) ? 'Да' : 'нет';
-    }
+replaceEnterUser();
     
-    function replaceUser(){ 
-        userFirst = userFirstName.replace(/[^a-zа-яA-ZА-Я]+/g, ''),
-        
-        userLast = userLastName.replace(/[^a-zа-яA-ZА-Я]+/g, ''),
-        
-        userSecond = userSecondName.replace(/[^a-zа-яA-ZА-Я]+/g, ''),
-        
-        userAge = userAge.replace(/[^0-9]/g, ''); 
-        }
+let newUserFirstName = userFirst[0].toUpperCase() + userFirst.toLowerCase(1).slice(1),
+   newUserLastName = userLast[0].toUpperCase() + userLast.toLowerCase(1).slice(1),
+   newUserSecondName = userSecond[0].toUpperCase() + userSecond.toLowerCase(1).slice(1);
     
-    
-    UserFirstName();
-    UserLastName();
-    UserSecondName();
-    UserAge();
-    UserGender();
-    replaceUser();
-    
-    
-    let newUserFirstName = userFirst[0].toUpperCase() + userFirst.toLowerCase(1).slice(1); 
-    let newUserLastName = userLast[0].toUpperCase() + userLast.toLowerCase(1).slice(1);
-    let newUserSecondName = userSecond[0].toUpperCase() + userSecond.toLowerCase(1).slice(1);
-    
-    alert(`    ФИО: ${newUserFirstName} ${newUserLastName} ${newUserSecondName} 
-    возраст, лет: ${userAge}
-    возраст, дней: ${userAge * 365}
-    через 5 лет вам будет: ${+userAge + 5}
-    ваш пол: ${userGat ? 'мужской' : 'женский'}
-    пенсионный возраст: ${userResult}`);
+alert(`    ФИО: ${newUserFirstName} ${newUserLastName} ${newUserSecondName} 
+возраст, лет: ${userAge}
+возраст, дней: ${userAge * 365}
+через 5 лет вам будет: ${userAg + 5}
+ваш пол: ${userGat ? 'мужской' : 'женский'}
+пенсионный возраст: ${userResult}`);
